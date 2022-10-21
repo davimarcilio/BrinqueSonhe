11973261498ad3d369561e203cc6bc5d53e46839
Davi Marcilio <davimarcilio.js@gmail.com>
2022-10-21 18:29:01 -0300

    Root paste resolved

--- routes/CSSRoutes.js
+++ routes/CSSRoutes.js
@@ -2,10 +2,9 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 router.get('/indexcss', function (req, res) {
-    console.log(rootpaste);
-    res.sendFile(path.join(rootpaste, 'views/styles/style.css'));
+    res.sendFile(path.join(__dirname, '../views/styles/style.css'));
 })
 router.get('/ABCA', function (req, res) {
-    res.sendFile(path.join(rootpaste, 'views/styles/AnimatedBackgrounds-CanvasAnimation.css'));
+    res.sendFile(path.join(__dirname, '../views/styles/AnimatedBackgrounds-CanvasAnimation.css'));
 })
 module.exports = router;
